b48ddb176415681ea7c0e64acee96966f7e5a89e
Rexi239 <113353063+YepFun@users.noreply.github.com>
2024-09-16 09:15:07 +0300

    lab1.1.4 done

--- sem1/phys_sem1/labs_sem1/lab1.1.4/group_data.py
+++ sem1/phys_sem1/labs_sem1/lab1.1.4/group_data.py
@@ -1,3 +1,5 @@
+import math
+
 def group_data(data):
     data_10 = [0] * 400
     i = 0
@@ -95,4 +97,70 @@ with open('lab_data_bare.txt') as f:
 data_10, data_20, data_40, data_80 = group_data(data)
 
 #print_tables(data_10, data_20, data_40, data_80)
-print_histogram_data(data_10, data_20, data_40, data_80)
+#print_histogram_data(data_10, data_20, data_40, data_80)
+
+sum_10, sum_20, sum_40, sum_80 = 0, 0, 0, 0
+sq_sum_10, sq_sum_20, sq_sum_40, sq_sum_80 = 0, 0, 0, 0
+for i in range(len(data_10)):
+    sum_10 += data_10[i]
+for i in range(len(data_20)):
+    sum_20 += data_20[i]
+for i in range(len(data_40)):
+    sum_40 += data_40[i]
+for i in range(len(data_80)):
+    sum_80 += data_80[i]
+print("sum:", sum_10, sum_20, sum_40, sum_80)
+average_10, average_20, average_40, average_80 = sum_10/400, sum_20/200, sum_40/100, sum_80/50
+print("average:", average_10, average_20, average_40, average_80)
+
+for i in range(len(data_10)):
+    sq_sum_10 += (data_10[i] - average_10) ** 2
+for i in range(len(data_20)):
+    sq_sum_20 += (data_20[i] - average_20) ** 2
+for i in range(len(data_40)):
+    sq_sum_40 += (data_40[i] - average_40) ** 2
+for i in range(len(data_80)):
+    sq_sum_80 += (data_80[i] - average_80) ** 2
+
+sq_sigma_10, sq_sigma_20, sq_sigma_40, sq_sigma_80 = sq_sum_10/400, sq_sum_20/200, sq_sum_40/100, sq_sum_80/50
+sigma_10, sigma_20, sigma_40, sigma_80 = math.sqrt(sq_sigma_10), math.sqrt(sq_sigma_20), math.sqrt(sq_sigma_40), math.sqrt(sq_sigma_80)
+print("sq_sum:", sq_sum_10, sq_sum_20, sq_sum_40, sq_sum_80)
+print("sq_sigma:", sq_sigma_10, sq_sigma_20, sq_sigma_40, sq_sigma_80)
+print("sigma:", sigma_10, sigma_20, sigma_40, sigma_80)
+print("2 * sigma:", 2 * sigma_10, 2 * sigma_20, 2 * sigma_40, 2 * sigma_80)
+
+print("sqrt_average:", math.sqrt(average_10), math.sqrt(average_20), math.sqrt(average_40), math.sqrt(average_80))
+print("")
+
+count_s_10, count_s_20, count_s_40, count_s_80 = 0, 0, 0, 0
+count_2s_10, count_2s_20, count_2s_40, count_2s_80 = 0, 0, 0, 0
+for i in range(len(data_10)):
+    if abs(data_10[i] - average_10) <= sigma_10:
+        count_s_10 += 1
+    if abs(data_10[i] - average_10) <= 2 * sigma_10:
+        count_2s_10 += 1
+for i in range(len(data_20)):
+    if abs(data_20[i] - average_20) <= sigma_20:
+        count_s_20 += 1
+    if abs(data_20[i] - average_20) <= 2 * sigma_20:
+        count_2s_20 += 1
+for i in range(len(data_40)):
+    if abs(data_40[i] - average_40) <= sigma_40:
+        count_s_40 += 1
+    if abs(data_40[i] - average_40) <= 2 * sigma_40:
+        count_2s_40 += 1
+for i in range(len(data_80)):
+    if abs(data_80[i] - average_80) <= sigma_80:
+        count_s_80 += 1
+    if abs(data_80[i] - average_80) <= 2 * sigma_80:
+        count_2s_80 += 1
+
+print("count_s:", count_s_10, count_s_20, count_s_40, count_s_80)
+print(count_s_10/400, count_s_20/200, count_s_40/100, count_s_80/50)
+print("count_2s:", count_2s_10, count_2s_20, count_2s_40, count_2s_80)
+print(count_2s_10/400, count_2s_20/200, count_2s_40/100, count_2s_80/50)
+
+sigma_st_10, sigma_st_20, sigma_st_40, sigma_st_80 = sigma_10/math.sqrt(400), sigma_20/math.sqrt(200), sigma_40/math.sqrt(100), sigma_80/math.sqrt(50)
+print("sigma_st:", sigma_st_10, sigma_st_20, sigma_st_40, sigma_st_80)
+
+print("e:", sigma_st_10/average_10, sigma_st_20/average_20, sigma_st_40/average_40, sigma_st_80/average_80)
